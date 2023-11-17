0900d6987480021199e156c9539a97a33b93c63c
LimLife <LimFobos@gamil.com>
2023-11-17 19:44:46 +0300

    add_grpc_client_to_blazor_wasm

--- src/Services/OrderManagementSystem/Model/DataBase/OrderDB.cs
+++ src/Services/OrderManagementSystem/Model/DataBase/OrderDB.cs
@@ -17,6 +17,21 @@ namespace OrderManagementSystem.Model.DataBase
             modelBuilder.ApplyConfiguration(new OrderItemConfiguration());
             modelBuilder.ApplyConfiguration(new ProviderConfiguration());
             modelBuilder.ApplyConfiguration(new OrderConfiguration());
+
+
+            //Add beginning data
+            modelBuilder.Entity<Provider>().HasData(new List<Provider> {
+                new Provider { Id = 1, Name = "At Alesya"},
+                new Provider { Id = 2, Name = "Golden Path"},
+                new Provider { Id = 3, Name = "Lizards"},
+                new Provider { Id = 4, Name = "Bagdan Inc"},
+                new Provider { Id = 5, Name = "Ancient Rus"},
+                new Provider { Id = 6, Name = "The Beast Company" },
+                new Provider { Id = 7, Name = "Trade Federation of Planets"},
+                new Provider { Id = 8, Name = "Another beast Company"},
+                new Provider { Id = 9, Name = "Horns and hooves"},
+                new Provider { Id = 10, Name = "Midas"}
+            });
         }
     }
 }
